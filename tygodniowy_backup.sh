6c0601185f26ea2ad638a8d9391b2d5296306b1d
al <33kkaarr@gmail.com>
2024-11-26 11:49:57 +0100

    Add a playbook, remove rm -rf

--- tygodniowy_backup.sh
+++ tygodniowy_backup.sh
@@ -15,7 +15,6 @@ tar --preserve-permissions -czf "$BACKUP_DIR/repo_git_2_backup_$TIMESTAMP.tar.gz
 git clone --mirror git@github.com:Szajszen/ask_repo1.git "$REPO1_DIR"
 tar --preserve-permissions -czf "$BACKUP_DIR/repo_git_1_backup_$TIMESTAMP.tar.gz" -C "$REPO1_DIR" .
 
-rm -rf "$REPO1_DIR"
 
 echo "Wykonano tygodniowy backup"
 
